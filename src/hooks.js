6cc7adca03d03e7e51c26fa85aa21d20d61d2202
Tom Wilson <twilson63@gmail.com>
2021-06-01 19:45:44 +0000

    added session info

--- src/hooks.js
+++ src/hooks.js
@@ -1,22 +1,29 @@
 import cookie from 'cookie';
-import { v4 as uuid } from '@lukeed/uuid';
+//import { v4 as uuid } from '@lukeed/uuid';
+import { not, propEq } from 'ramda'
 
 export const handle = async ({ request, resolve }) => {
-	const cookies = cookie.parse(request.headers.cookie || '');
-	request.locals.userid = cookies.userid || uuid();
+	const cookies = cookie.parse(request.headers.cookie || '')
+	request.locals.username = not(propEq('data', 'deleted', cookies)) ? cookies.data : ''
 
 	// TODO https://github.com/sveltejs/kit/issues/1046
 	if (request.query.has('_method')) {
-		request.method = request.query.get('_method').toUpperCase();
+		request.method = request.query.get('_method').toUpperCase()
 	}
 
-	const response = await resolve(request);
+	const response = await resolve(request)
 
-	if (!cookies.userid) {
+	if (!cookies.data) {
 		// if this is the first time the user has visited this app,
 		// set a cookie so that we recognise them when they return
-		response.headers['set-cookie'] = `userid=${request.locals.userid}; Path=/; HttpOnly`;
+		response.headers['set-cookie'] = `data=${request.locals.username}; Path=/; HttpOnly`
 	}
 
-	return response;
-};
+	return response
+}
+
+export async function getSession(request) {
+	return {
+		username: request.locals.username
+	}
+}
